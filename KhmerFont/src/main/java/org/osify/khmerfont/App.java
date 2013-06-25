67d8b32765f518f7a816227f4416462c7b937748
seang.kimsuor@gmail.com <seang.kimsuor@gmail.com@eaecb938-7166-1488-3768-b3feadd13d71>
2013-06-25 11:40:01 +0000

    get postscript

--- KhmerFont/src/main/java/org/osify/khmerfont/App.java
+++ KhmerFont/src/main/java/org/osify/khmerfont/App.java
@@ -1,13 +1,43 @@
 package org.osify.khmerfont;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.fontbox.cff.Type1FontFormatter;
+import org.apache.fontbox.ttf.HeaderTable;
+import org.apache.fontbox.ttf.PostScriptTable;
+import org.apache.fontbox.ttf.TTFParser;
+import org.apache.fontbox.ttf.TTFTable;
+import org.apache.fontbox.ttf.TrueTypeFont;
+
+import sun.font.TrueTypeGlyphMapper;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    final static String FILE_NAME = "E:\\Work\\Java\\Mine\\Spring\\KhmerFont\\src\\main\\resources\\data\\Kh-Battambang.ttf";    
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+      TTFParser parser = new TTFParser();
+      try {
+          
+        TrueTypeFont font = parser.parseTTF(new File(FILE_NAME));
+        PostScriptTable postScriptTable = font.getPostScript();
+        for (String phName : postScriptTable.getGlyphNames()) {
+            System.out.println(phName);    
+        }
+        
+    } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+      
     }
 }
